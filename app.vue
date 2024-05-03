bdb4dff385a759bf69d6fd0787c35f5525049d1a
xAksssenov <aks.kirill32@gmail.com>
2024-05-03 16:35:21 +0300

    feat: header and fix some problems with other files

--- app.vue
+++ app.vue
@@ -1,5 +1,78 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
+
+<style lang="scss">
+* {
+    padding: 0;
+    margin: 0;
+    font-size: 1rem;
+}
+
+input,
+textarea {
+    user-select: text;
+}
+
+html {
+    font-size: 62.5%;
+    overflow-x: hidden;
+    background-color: rgba(238 231 225 / 100%);
+    transition: all 0.5s ease-in-out;
+}
+
+body {
+    font-family: Inter, sans-serif;
+    max-width: 1920px;
+    margin: 3.6rem 9rem 2.7rem;
+}
+
+h1 {
+    font-size: 1.4rem;
+}
+
+h2 {
+    font-size: 1.3rem;
+}
+
+h3 {
+    font-size: 1.2rem;
+}
+
+h4 {
+    font-size: 1.1rem;
+}
+
+h5 {
+    font-size: 1rem;
+}
+
+a {
+    text-decoration: none;
+    outline: none;
+}
+
+input {
+    display: block;
+    box-sizing: border-box;
+    background: none;
+    border: none;
+    border-radius: 0.15rem;
+    outline: none;
+    border-radius: 1.4rem;
+}
+
+button {
+    display: block;
+    box-sizing: border-box;
+    text-align: center;
+    background: none;
+    border: none;
+    border-radius: 0.2rem;
+    outline: none;
+    cursor: pointer;
+    transition: 100ms;
+}
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,72 @@
+<template>
+    <header class="header">
+        <div class="header__naming">
+            <SvgoIconLogo class="header__naming--logo" filled />
+            <h3 class="header__naming--name">Институт запусков</h3>
+        </div>
+
+        <button class="header__button">
+            <SvgoIconAvatar class="header__button--avatar" filled />
+
+            <div class="header__login">
+                <p class="header__login--text">Войти</p>
+                <SvgoIconArrow class="header__login--icon" filled />
+            </div>
+        </button>
+    </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.header {
+    display: flex;
+    justify-content: space-between;
+
+    &__naming {
+        display: flex;
+        align-items: center;
+        gap: 1.6rem;
+
+        &--logo {
+            width: 4.8rem;
+            height: 4.8rem;
+        }
+
+        &--name {
+            font-family: Inter, sans-serif;
+            font-size: 2.5rem;
+            color: rgba(145 16 22 / 100%);
+        }
+    }
+
+    &__button {
+        display: flex;
+        align-items: center;
+        gap: 1.6rem;
+        padding: 1.2rem 1.6rem;
+        background-color: white;
+        box-shadow: rgba(0 0 0 / 5%);
+        border-radius: 1.6rem;
+
+        &--avatar {
+            width: 3.6rem;
+            height: 3.6rem;
+        }
+    }
+
+    &__login {
+        display: flex;
+        align-items: center;
+        gap: 3.6rem;
+
+        &--text {
+            font-size: 1.6rem;
+        }
+
+        &--icon {
+            width: 13.39px;
+        }
+    }
+}
+</style>
