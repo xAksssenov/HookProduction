aa0e55a8d1274929200322378cd2700a51b86891
xAksssenov <aks.kirill32@gmail.com>
2024-05-03 15:54:20 +0300

    feat: add architecture

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+      head: {
+          title: 'Aksenov Kirill',
+          charset: 'utf-8',
+          viewport: 'width=device-width, initial-scale=1',
+      },
+  },
+  components: [
+      {
+          path: '~/components',
+          pathPrefix: false,
+      },
+  ],
+  devtools: { enabled: true },
+  css: ['~/assets/scss/main.css'],
+  modules: [
+      '@nuxtjs/eslint-module',
+      '@nuxtjs/stylelint-module',
+      'nuxt-svgo',
+  ],
+  svgo: {
+      autoImportPath: '~/assets/icons/',
+  },
+  runtimeConfig: {
+      public: {
+          apiBase: 'http://localhost:3000',
+      },
+  },
 })
